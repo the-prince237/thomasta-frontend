bec00bf4407aaee20cb67e92c77ebb032afd30b3
Durin TASONDOCK <temgoua484@gmail.com>
2023-01-09 20:17:40 +0100

    adding structures for accommodations, account, billing, ceremonies, leisures, magazines, school support

--- src/views/Account/UserAccount.vue
+++ src/views/Account/UserAccount.vue
@@ -1,6 +1,32 @@
 <template>
   <div>
-    account
+    <h2>compte</h2>
+    <section>
+      <h3>Zone de navigation</h3>
+      <h4>Activités</h4>
+      <h4>Messageries</h4>
+      <h4>Espace Pro</h4>
+    </section>
+    <section>
+      <h3>Avtivités</h3>
+      <h4>liste des activité de l'utilisateur dans chaque domaine :</h4>
+      <ul>
+        <li><b>hébergement</b></li>
+        <li><b>lessons</b></li>
+        <li><b>magazines</b></li>
+        <li><b>boutique (panier, historique des commandes)</b></li>
+        <li><b>professionnels, lieux de loisirs et cérémoniels contactés</b></li>
+      </ul>
+    </section>
+    <section>
+      <h3>Messageries</h3>
+      <h4>filtre des conversations en fonction du type d'inerlocuteur</h4>
+      <h4>Liste des conversations</h4>
+      <h5>simulation des conversations intéractives</h5>
+    </section>
+    <section>
+      <h3>Espace pro</h3>
+    </section>
   </div>
 </template>
 
--- src/views/Billing/RegBilling.vue
+++ src/views/Billing/RegBilling.vue
@@ -1,6 +1,18 @@
 <template>
   <div>
-    page
+    <h2>Facturation</h2>
+    <section>
+      <h3>Résumé de la commande</h3>
+      <p>Résumé des informations de registration sur les services payants</p>
+    </section>
+    <section>
+      <h3>Options de facturation</h3>
+      <ul>
+        <li>mode de payement</li>
+        <li>informations de moyen de payement</li>
+        <li>plan sélectionné</li>
+      </ul>
+    </section>
   </div>
 </template>
 
