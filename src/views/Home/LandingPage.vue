916d51628a65bd86e2eb1e15059da1d7cabc7168
Durin TASONDOCK <temgoua484@gmail.com>
2023-01-09 04:17:54 +0100

    structuration de la page d'accueil avec une présentation des services de l'entreprise

--- src/views/Home/LandingPage.vue
+++ src/views/Home/LandingPage.vue
@@ -1,6 +1,62 @@
 <template>
   <div>
-    home
+    <section>
+      <h2>Hero section</h2>
+      <div>
+        <div>
+          <h3>Titre de la section</h3>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi vero doloribus accusamus tempore nam veritatis, illo ducimus odit! Similique est doloribus illum. Aliquid praesentium delectus ullam nisi iure et rem.
+          </p>
+        </div>
+        <img src="#" alt="">
+      </div>
+    </section>
+    <section>
+      <h2>Annuaire des professionnels</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit nam rerum veniam, repudiandae rem vitae sit iure eaque, repellendus illo vel reprehenderit optio sunt porro quibusdam quae velit ad. Numquam?</p>
+    </section>
+    <section>
+      <h2>Boutique</h2> 
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit nam rerum veniam, repudiandae rem vitae sit iure eaque, repellendus illo vel reprehenderit optio sunt porro quibusdam quae velit ad. Numquam?</p>
+    </section>
+    <section>
+      <h2>cta contacts</h2> 
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit nam rerum veniam, repudiandae rem vitae sit iure eaque, repellendus illo vel reprehenderit optio sunt porro quibusdam quae velit ad. Numquam?</p>
+    </section>
+    <section>
+      <h2>Education</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit nam rerum veniam, repudiandae rem vitae sit iure eaque, repellendus illo vel reprehenderit optio sunt porro quibusdam quae velit ad. Numquam?</p>
+      <h3>Formation</h3>
+      <h3>Soutien scholaire</h3>
+    </section>
+    <section>
+      <h2>Hébergement</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit nam rerum veniam, repudiandae rem vitae sit iure eaque, repellendus illo vel reprehenderit optio sunt porro quibusdam quae velit ad. Numquam?</p>
+      <h3>Hôtels</h3>
+      <h3>Motels</h3>
+      <h3>Auberges</h3>
+    </section>
+    <section>
+      <h2>cta contacts</h2> 
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit nam rerum veniam, repudiandae rem vitae sit iure eaque, repellendus illo vel reprehenderit optio sunt porro quibusdam quae velit ad. Numquam?</p>
+    </section>
+    <section>
+      <h2>Lieu de loisirs</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit nam rerum veniam, repudiandae rem vitae sit iure eaque, repellendus illo vel reprehenderit optio sunt porro quibusdam quae velit ad. Numquam?</p>
+    </section>
+    <section>
+      <h2>Magazines</h2> 
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit nam rerum veniam, repudiandae rem vitae sit iure eaque, repellendus illo vel reprehenderit optio sunt porro quibusdam quae velit ad. Numquam?</p>
+    </section>
+    <section>
+      <h2>Salles de cérémonies</h2> 
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit nam rerum veniam, repudiandae rem vitae sit iure eaque, repellendus illo vel reprehenderit optio sunt porro quibusdam quae velit ad. Numquam?</p>
+    </section>
+    <section>
+      <h2>Formulaire de contact</h2> 
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit nam rerum veniam, repudiandae rem vitae sit iure eaque, repellendus illo vel reprehenderit optio sunt porro quibusdam quae velit ad. Numquam?</p>
+    </section>
   </div>
 </template>
 
